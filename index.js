21e7af6530df8478d1151b2790fee2b0253c930c
Antonio <76496876+acavlovic@users.noreply.github.com>
2023-12-15 22:23:30 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+let insertedValue;
+const inputEl = document.getElementById("input-el");
+const convertBtn = document.getElementById("convert-btn");
+const lengthEl = document.getElementById("length-el");
+const volumenEl = document.getElementById("volumen-el");
+const massEl = document.getElementById("mass-el");
+
+function render(value) {
+  console.log(insertedValue);
+  let listResult = "";
+  const feet = insertedValue * 3.28084;
+  const meters = insertedValue / 3.28084;
+  const liters = insertedValue * 0.2464;
+  const gallons = insertedValue / 0.2464;
+  const kilograms = insertedValue * 2.204;
+  const pounds = insertedValue / 2.204;
+
+  if (value >= 0) {
+    lengthEl.innerHTML = `<p>${value} meters = ${feet.toFixed(
+      3
+    )} feet | ${value} feet = ${meters.toFixed(3)} meters</p>`;
+    volumenEl.innerHTML = `<p>${value} liters = ${gallons.toFixed(
+      3
+    )} | ${value} gallons = ${liters.toFixed(3)}</p>`;
+    massEl.innerHTML = `<p>${value} kilograms = ${pounds.toFixed(
+      3
+    )} | ${value} pounds = ${kilograms.toFixed(3)}</p>`;
+  }
+}
+
+convertBtn.addEventListener("click", function () {
+  insertedValue = parseFloat(inputEl.value);
+  inputEl.value = "";
+  render(insertedValue);
+});
